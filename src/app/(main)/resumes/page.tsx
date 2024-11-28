1efc4259bf1717eca82cd9b9ea145417dabb2f92
Maor <maorsa9@gmail.com>
2024-11-28 19:13:34 +0200

    Global modal/dialog with Zustand

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,11 +1,9 @@
-import Link from "next/link";
-import { Metadata } from "next";
-import { PlusSquare } from "lucide-react";
 import { auth } from "@clerk/nextjs/server";
+import { Metadata } from "next";
 
-import { Button } from "@/components/ui/button";
 import { resumeDataInclude } from "@/lib/types";
 import prisma from "@/lib/prisma";
+import CreateResumeButton from "./CreateResumeButton";
 import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
@@ -40,12 +38,7 @@ export default async function Page() {
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New resume
-        </Link>
-      </Button>
+      <CreateResumeButton canCreate={totalCount < 3} />
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your resumes</h1>
         <p>Total: {totalCount}</p>
