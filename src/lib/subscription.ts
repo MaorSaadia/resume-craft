8f3d89cac1403d29d0baac3342b674759ab3a366
Maor <maorsa9@gmail.com>
2024-12-02 12:58:48 +0200

    Save/fetch subscriptions (React cache, context provider)

---
+++ src/lib/subscription.ts
@@ -0,0 +1,39 @@
+import { cache } from "react";
+
+import { env } from "@/env";
+import prisma from "./prisma";
+
+export type SubscriptionLevel = "free" | "pro" | "pro_plus";
+
+export const getUserSubscriptionLevel = cache(
+  async (userId: string): Promise<SubscriptionLevel> => {
+    const subscription = await prisma.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (
+      !subscription ||
+      subscription.lemonSqueezyCurrentPeriodEnd < new Date()
+    ) {
+      return "free";
+    }
+
+    if (
+      subscription.lemonSqueezyPriceId ===
+      env.NEXT_PUBLIC_LEMON_SQUEEZY_VARIANT_ID_PRO_MONTHLY
+    ) {
+      return "pro";
+    }
+
+    if (
+      subscription.lemonSqueezyPriceId ===
+      env.NEXT_PUBLIC_LEMON_SQUEEZY_VARIANT_ID_PRO_PLUS_MONTHLY
+    ) {
+      return "pro_plus";
+    }
+
+    throw new Error("Invalid subscription");
+  },
+);
